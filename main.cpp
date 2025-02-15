908f37c693dd8dd620a47a8d0715100aeae5e00e
developer <developer@iamroot.ru>
2025-02-15 13:09:46 +0700

    Реализация методов класса serial_port

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <QCoreApplication>
 #include <QtSerialPort/QSerialPortInfo>
+#include "serial_port.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     return a.exec();
+    serial_port port;
 }
--- serial_port.cpp
+++ serial_port.cpp
@@ -1,27 +1,52 @@
 #include "serial_port.h"
 
-serial_port::serial_port() {}
+serial_port::serial_port()  {}
 serial_port::~serial_port() {}
 
-void serial_port::device_detection(const unsigned int vid, const unsigned int pid)
+bool serial_port::device_detection(const unsigned int vid, const unsigned int pid)
 {
+    foreach(const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
+    {
+        if(info.vendorIdentifier() == vid and info.productIdentifier()== pid)
+        {
+            this->name          = info.portName();
+            this->serial_number = info.serialNumber();
+            return true;
+        }
+    }
+    return false;
 }
 
 bool serial_port::open_()
 {
-    return true;
+    this->setPortName(this->name);
+    this->setBaudRate(QSerialPort::Baud115200);
+
+    if(!this->open(QIODevice::ReadWrite))
+        return false;
+    else
+    {
+        this->readAll();
+        return true;
+    }
 }
 
 bool serial_port::close_()
 {
-    return true;
+    this->close();
+
+    if(this->isOpen() == false)
+        return true;
+
+    return false;
 }
 
-void    serial_port::resive_data()
+void serial_port::send_data(const char *buf, unsigned int len)
 {
+    this->write(buf, len);
 }
 
-void    serial_port::send_data()
+QString serial_port::get_serial_number() const
 {
-
+    return this->serial_number;
 }
