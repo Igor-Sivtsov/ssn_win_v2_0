2f648a4e44dc18ad136bc5dc8eedf04e894de514
developer <developer@iamroot.ru>
2025-02-15 13:18:09 +0700

    Удален main.h

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include <QtSerialPort/QSerialPortInfo>
 #include "serial_port.h"
 
+enum class text_color
+{
+    blue    = 1,
+    green   = 2,
+    red     = 4,
+    white   = 15
+};
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
